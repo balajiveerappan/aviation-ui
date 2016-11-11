3df60f984b4a2c670fd538aaa8bf6e1d50d0a22d
Amit Bansal <amitbansal26@gmail.com>
2016-11-11 07:48:20 +0530

    updated

--- src/main/java/com/aviation/poc/AviationUiApplication.java
+++ src/main/java/com/aviation/poc/AviationUiApplication.java
@@ -1,9 +1,7 @@
 package com.aviation.poc;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -13,15 +11,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableEurekaClient
 public class AviationUiApplication extends SpringBootServletInitializer {
 
-	@Value("${services.auth}")
-	private String authService;
-
-
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AviationUiApplication.class);
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AviationUiApplication.class, args);
